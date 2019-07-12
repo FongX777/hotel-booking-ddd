2594a69bba2ee745fa50677bb07f3bf75d2c114f
fong <fong@meepshop.com>
2019-07-12 18:05:59 +0800

    Complete room

--- src/booking/domain/room/room.ts
+++ src/booking/domain/room/room.ts
@@ -19,6 +19,78 @@ export interface RoomProps {
 export class Room extends Entity<RoomProps> {
   private constructor(props: RoomProps) {
     super(props);
+    if (this.props.numberOfOccupants <= 0) {
+      throw new Error('');
+    }
+    if (this.props.size <= 0) {
+      throw new Error('');
+    }
+    if (this.props.price <= 0) {
+      throw new Error('');
+    }
+  }
+
+  updateName(name: string) {
+    this.props.name = name;
+  }
+
+  updateRoomNo(roomNo: string) {
+    this.props.roomNo = roomNo;
+  }
+
+  updateDescription(description: string) {
+    this.props.description = description;
+  }
+
+  updateNumberOfOccupants(numberOfOccupants: number) {
+    this.props.numberOfOccupants = numberOfOccupants;
+  }
+
+  updateSize(size: number) {
+    this.props.size = size;
+  }
+
+  updatePrice(price: number) {
+    this.props.price = price;
+  }
+
+  updateCoverImage(image: string) {
+    this.props.coverImage = image;
+  }
+
+  activate() {
+    this.props.active = true;
+  }
+
+  deactivate() {
+    this.props.active = false;
+  }
+
+  addFacilities(facilities: string[]) {
+    this.props.facilities.push(...facilities);
+  }
+
+  removeFacilities(facilities: string[]) {
+    const newFacilities: string[] = [];
+    this.props.facilities.forEach(facility => {
+      if (!facilities.includes(facility)) {
+        newFacilities.push(facility);
+      }
+    });
+    this.props.facilities = newFacilities;
+  }
+
+  addImages(images: string[]) {
+    this.props.images.push(...images);
+  }
+  removeImages(images: string[]) {
+    const newImages: string[] = [];
+    this.props.images.forEach(image => {
+      if (!images.includes(image)) {
+        newImages.push(image);
+      }
+    });
+    this.props.images = newImages;
   }
 
   static create(params: {
