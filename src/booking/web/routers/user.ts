4ae2df546b26b15efc8228c13dc12eeabb7e91f0
fong <fong1143@gmail.com>
2019-07-16 22:33:55 +0800

    Wire up register user

--- src/booking/web/routers/user.ts
+++ src/booking/web/routers/user.ts
@@ -2,11 +2,37 @@
  * https://expressjs.com/zh-tw/guide/using-middleware.html
  */
 import { Router } from 'express';
+import {
+  RegisterUserUsecase,
+  RegisterUserUsecaseOutput,
+  RegisterUserUsecaseInput
+} from '../../usecase/user';
+import { UserRepositoryInMem } from '../../adapter/repository/user/repository';
+import bcrypt from '../../infrastructure/bcrypt';
 const router = Router();
 
 router.get('/:id', function(req, res) {
   const id = req.params.id;
   res.send(id);
+});
+
+router.post('/register', function(req, res) {
+  const { name, email, password, mobilePhone } = req.body;
+  const input = new RegisterUserUsecaseInput({
+    name,
+    email,
+    password,
+    mobilePhone
+  });
+  const repo = new UserRepositoryInMem();
+  const usecase = new RegisterUserUsecase(repo, bcrypt);
+  const output = usecase.execute(input);
+
+  if (output) {
+    res.redirect('/');
+  } else {
+    res.status(500).send('register error');
+  }
 });
 
 export default router;
