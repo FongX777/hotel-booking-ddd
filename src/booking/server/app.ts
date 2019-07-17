e11cf4384f6822a0ba988da0488e888a2c10ca21
fong <fong@meepshop.com>
2019-07-17 16:55:16 +0800

    Refactor server

---
+++ src/booking/server/app.ts
@@ -0,0 +1,40 @@
+// lib/app.ts
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import { createUserRoute } from './routers/user';
+import IUserRepository from '../usecase/user/i-repository';
+
+export interface Config {
+  port: number;
+}
+
+export interface ExpressApp {
+  start: () => void;
+}
+
+export interface Repositories {
+  user: IUserRepository;
+}
+
+export function createExpressServerApp(
+  config: Config,
+  repos: Repositories
+): ExpressApp {
+  const app: express.Application = express();
+
+  app.use(bodyParser.json());
+  app.use('/user', createUserRoute(repos.user));
+
+  app.get('/', function(req, res) {
+    res.send('Hello World!');
+  });
+
+  const expressApp: ExpressApp = {
+    start: () =>
+      app.listen(config.port, () => {
+        console.log('Example app listening on port 3000!');
+      })
+  };
+  return expressApp;
+}
