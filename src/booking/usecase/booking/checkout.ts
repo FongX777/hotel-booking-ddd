3b7f4e66ef5424618f93d365f5db35117f491959
Chigun <hankle6606@gmail.com>
2019-08-13 19:58:09 +0800

    <add> usecase-booking : checkin, checkout, close

---
+++ src/booking/usecase/booking/checkout.ts
@@ -0,0 +1,27 @@
+import {BookingRepository} from './repository';
+import {BookingId} from '../../domain/model/booking/booking';
+
+export class CheckoutBookingUsecase{
+    private readonly bookingRepo:BookingRepository;
+    constructor(bookingRepo:BookingRepository){
+        this.bookingRepo = bookingRepo;
+    }
+    execute(input:CheckoutBookingInput,output:CheckoutBookingOutput){
+        const {id} = input;
+        const bookingId = new BookingId(id);
+        const booking =this.bookingRepo.findById(bookingId);
+        if(booking===undefined){
+            output.success=false;
+        }
+        else{
+            booking.checkout();
+            output.success=true;
+        }
+    }
+}
+export type CheckoutBookingInput={
+    id:string;
+}
+export type CheckoutBookingOutput ={
+    success?:boolean;
+}
--- src/booking/usecase/booking/index.ts
+++ src/booking/usecase/booking/index.ts
@@ -3,3 +3,6 @@ export * from './repository';
 export * from './get';
 export * from './edit';
 export * from './pay';
+export * from './checkin';
+export * from './checkout';
+export * from './close';
